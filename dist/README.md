ec343e7a60d793a4367871876a3b63468b4a3af2
GunnySensei <gunnerwagoner@hotmail.com>
2022-01-15 16:05:52 -0600

    fixed user imput, added printing to README page

---
+++ dist/README.md
@@ -0,0 +1,38 @@
+
+GNU AGPLv3  
+
+# Generator  
+
+## Description of Project  
+- Generates a README.  
+
+##Table of Contents  
+- [Installation](#Installation)  
+- [Usage](#Usage)  
+- [License](#License)  
+- [Contributing](#Contributing)  
+- [Tests](#Tests)  
+- [Questions](#Questions)  
+
+# Installation  
+- How to install this application:  
+-- run Node  
+
+# Usage  
+use it anywhere  
+
+# License  
+GNU AGPLv3  
+# Contributing  
+- How to contribute ask me below  
+
+# Tests  
+- Tests are used: true  
+- How to test this application:  
+-- Jest  
+
+# Questions
+- If you have further questions, feel free to see my contact information below:  
+-- [GitHub](https://github.com/GunnySensei)  
+-- Email: gunnerwagoner@gmail.com  
+    
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { rejects } = require('assert');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
+const { resolve } = require('path');
 
 const promptUser = () => {
     console.log(`
@@ -43,6 +46,39 @@ const promptUser = () => {
             type: 'input',
             name: 'usage',
             message: 'Enter the usage information for your repository'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your repo:',
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter how others can contribute to the project'
+        },
+        {
+            type: 'confirm',
+            name: 'testsConfirm',
+            message: 'Are tests implemented?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter how to implement tests on this project:',
+            when: ({ testsConfirm }) => testsConfirm
+        },
+        {
+            type: 'input',
+            name: 'gitHubUser',
+            message: 'Enter your GitHub username:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email for contacting',
         }
     ])
 
@@ -51,27 +87,64 @@ const promptUser = () => {
         return repoData;
     }) 
     .then(repoData => {
-        generatePage(repoData)});
+        const completedMarkdown = generatePage(repoData)
+        console.log(completedMarkdown);
+        writeFile(completedMarkdown);
+    })
 
     
 };
 
-const generatePage = (repoData) => {
-    console.log(repoData);
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/README.md', repoData, err => {
-            if(err) {
-                reject(err);
-                return;
-            }
+const generatePage = repoData => {
+    const { title, description, installation, usage, license, contributing, testsConfirm, tests, gitHubUser, email } = repoData;
+    return `
+${license}  
 
-            resolve({
-                ok: true,
-                message: 'File Created!'
-            });
+# ${title}  
+
+## Description of Project  
+- ${description}.  
+
+##Table of Contents  
+- [Installation](#Installation)  
+- [Usage](#Usage)  
+- [License](#License)  
+- [Contributing](#Contributing)  
+- [Tests](#Tests)  
+- [Questions](#Questions)  
+
+# Installation  
+- How to install this application:  
+-- ${installation}  
+
+# Usage  
+${usage}  
+
+# License  
+${license}  
+# Contributing  
+- How to contribute ${contributing}  
+
+# Tests  
+- Tests are used: ${testsConfirm}  
+- How to test this application:  
+-- ${tests}  
+
+# Questions
+- If you have further questions, feel free to see my contact information below:  
+-- [GitHub](https://github.com/${gitHubUser})  
+-- Email: ${email}  
+    `
+}
+
+const writeFile = (data) => {
+    fs.writeFile('./dist/README.md', data, err => {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Document has been created!");
         });
-    });
-};
+    };
 
-promptUser();
+promptUser()
 
